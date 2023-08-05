9184388ed8d905f7158c4c6bc50d194c210ec768
보니 <73548875+Guboneui@users.noreply.github.com>
2023-08-06 03:57:14 +0900

    [FEAT] Swift Lint First Commit

---
+++ Practice-SwiftLint/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Practice-SwiftLint
+//
+//  Created by 구본의 on 2023/08/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    self.view.backgroundColor = .white
+    print("🔊[DEBUG]: ViewController - ViewDidLoad")
+  }
+}
