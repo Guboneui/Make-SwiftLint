cec226d14e1071a79601bb7b74dbd4ba09706c88
보니 <73548875+Guboneui@users.noreply.github.com>
2023-08-06 15:32:01 +0900

    [FEAT] Array `isEmpty` 체크

--- Practice-SwiftLint/ViewController.swift
+++ Practice-SwiftLint/ViewController.swift
@@ -9,9 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  private let arr: [Int] = [1, 2, 3, 4, 5]
+
   override func viewDidLoad() {
     super.viewDidLoad()
     self.view.backgroundColor = .white
     print("🔊[DEBUG]: ViewController - ViewDidLoad")
+  }
+
+  private func checkArrayIsEmpty() -> Bool {
+    if arr.isEmpty {
+      return true
+    } else {
+      return false
+    }
   }
 }
--- README.md
+++ README.md
@@ -8,4 +8,8 @@
 excluded:
   - 프로젝트/AppDelegate.swift
   - 프로젝트/SceneDelegate.swift
+  
+opt_in_rules:
+  # `array.count == 0` 보다 `array.isEmpty` 사용을 권장. `array.count == 0` 사용 시 에러 발생
+  - empty_count
 ```
